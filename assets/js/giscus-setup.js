bd4bfe023f2f38d296d5c079f056fa70a4cf3935
Ann-Huang-0 <zixiang.huang@mail.mcgill.ca>
2026-01-13 22:01:50 +0000

    Deploying to gh-pages from @ Ann-Huang-0/Ann-Huang-0.github.io@f795f74f1053e62f4cafe72123b8e0ef9f5b2869 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Ann-Huang-0/annhuang.github.io",
+    "data-repo": "Ann-Huang-0/Ann-Huang-0.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
